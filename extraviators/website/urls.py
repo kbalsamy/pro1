8108d9d7559f89a9aaa2509a0f47acde49e6a87d
Karthikeyan <bkarthikonmail@gmail.com>
2019-08-06 17:44:40 +0530

    working with django

---
+++ extraviators/website/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import homePage_view, pageNavigation_view
+
+app_name = 'website'
+
+
+urlpatterns = [path('', homePage_view, name='home'),
+               path('pets', pageNavigation_view, name='pets'),
+               path('excessbaggage', pageNavigation_view, name='baggage'),
+               path('repatriations', pageNavigation_view, name='hum'),
+               path('contact', pageNavigation_view, name='contact'),
+               ]
--- extraviators/website/views.py
+++ extraviators/website/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homePage_view(request):
+
+    return render(request, 'website/home.html',)
+
+
+def pageNavigation_view(request):
+
+    context = {}
+
+    path_params = {'/pets': 'website/pets.html',
+                   '/excessbaggage': 'website/baggage.html',
+                   '/repatriations': 'website/repatriations.html',
+                   '/contact': 'website/contact.html'}
+
+    if request.path in path_params.keys():
+
+        return render(request, path_params[request.path], context)
