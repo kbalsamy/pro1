543b431c59424f2682f42a37588be042d9b55ca0
Karthikeyan <bkarthikonmail@gmail.com>
2019-08-12 18:11:30 +0530

    finish it today

--- extraviators/website/models.py
+++ extraviators/website/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ExcessBaggageEnquiry(models.Model):
+
+    destination = models.CharField(max_length=50)
+    destination_city = models.CharField(max_length=50)
+    origin = models.CharField(max_length=50)
+    origin_city = models.CharField(max_length=50)
+    fullname = models.CharField(max_length=50)
+    email = models.EmailField()
+    Baggage_detail = models.TextField()
+    created_on = models.DateField(auto_now=True)
+
+    def __str__(self):
+
+        return str(self.id)
+
+
+class PetsRelocationEnquiry(models.Model):
+
+    destination = models.CharField(max_length=50)
+    destination_city = models.CharField(max_length=50)
+    origin = models.CharField(max_length=50)
+    origin_city = models.CharField(max_length=50)
+    saluatation = models.CharField(max_length=5)
+    firstname = models.CharField(max_length=50)
+    firstname = models.CharField(max_length=50)
+    email = models.EmailField()
+    contact_number = models.IntegerField()
+    Baggage_detail = models.TextField()
+    created_on = models.DateField(auto_now=True)
+
+    def __str__(self):
+
+        return str(self.id)
